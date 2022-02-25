b1b6c8dea15c1b8127a0cd3e288e3a9606680c83
Junya Kosaka <kosakajunya@gmail.com>
2022-02-25 22:25:58 +0900

    feat: t_exp_strlist_type

--- includes/expansion.h
+++ includes/expansion.h
@@ -6,10 +6,22 @@
 # include "parser.h"
 # include "libft.h"
 
+typedef enum e_exp_strlist_type
+{
+	SQUOTE,
+	DQUOTE,
+	ENV,
+	STRING,
+	STRING_SPACE,
+	NAKED_SPACE
+}				t_exp_strlist_type;
+
 typedef struct s_exp_strlist
 {
-	int				len;
-	char			*str;
+	int						len;
+	char					*str;
+	bool					in_squote;
+	bool					in_dquote;
 	struct s_exp_strlist	*next;
 }	t_exp_strlist;
 
