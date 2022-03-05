0385e428285d06b71221822b5c25879e92827ec3
Junya Kosaka <kosakajunya@gmail.com>
2022-03-05 14:05:31 +0900

    test: gtest directory

---
+++ test/gtest/execution.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+	#include "lexer.h"
+	#include "parser.h"
+	#include "expansion.h"
+	#include "execution.h"
+	#include "libft.h"
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <unistd.h>
+}
+
+extern char	**environ;
+
+void	func()
+{
+	EXPECT_EQ(system("diff outfile outfile_bash"), 0);
+}
+
+TEST(execution, execution_test00)
+{
+	char		*input		= "echo aa | cat > outfile";
+	char		*input_bash	= "echo aa | cat > outfile_bash";
+	t_node		*expression_list = expansion(input);
+
+	execution(expression_list);
+	func();
+}
