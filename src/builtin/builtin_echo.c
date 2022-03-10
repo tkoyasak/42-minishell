e7d13912f812b43a8fc41a71aab8f9b56cf4cb68
Junya Kosaka <kosakajunya@gmail.com>
2022-03-10 11:20:25 +0900

    refactor: use write_in_process in builtin_echo

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -12,18 +12,7 @@ int	builtin_echo(t_expression *expression, t_process *process, int cmd_idx)
 		itr = itr->next;
 		if (itr)
 			str = ft_strjoin(str, " ");
-		if (process->kind[1] == NONE)
-		{
-			// redirection なし
-			if (cmd_idx < expression->process_cnt - 1)
-				write(expression->pipefd[cmd_idx][1], str, ft_strlen(str));
-			else
-				write(STDOUT_FILENO, str, ft_strlen(str));
-		}
-		else
-		{
-			// redirection あり OUTPUTとAPPEND共通
-			write(process->fd[1], str, ft_strlen(str));
-		}
+		write_in_process(expression, process, cmd_idx, str);
 	}
+	return (0);
 }
