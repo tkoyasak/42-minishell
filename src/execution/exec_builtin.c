2f3d22d79cb7fd4f63b025f26515c56307757158
Junya Kosaka <kosakajunya@gmail.com>
2022-03-12 18:06:03 +0900

    feat: is_builtin

--- src/execution/exec_builtin.c
+++ src/execution/exec_builtin.c
@@ -1,50 +1,39 @@
 #include "execution.h"
 #include "builtin.h"
 
-// int (*builtin_func[]) (char **) = {
-//   &lsh_cd,
-//   &lsh_help,
-//   &lsh_exit,
-//   &lsh_echo,
-//   &lsh_pwd,
-//   &lsh_export,
-//   &lsh_unset,
-//   &lsh_env
-// };
+bool	is_builtin(char *cmd)
+{
+	const char *builtin_str[] = \
+			{"echo", "cd", "pwd", "export", "unset", "env", "exit"};
+	int idx;
 
-// int lsh_execute(char **args)
-// {
-//   int i;
+	idx = 0;
+	while (idx < 7)
+	{
+		if (ft_strcmp(cmd, builtin_str[idx]) == 0)
+			return (true);
+		idx++;
+	}
+	return (false);
+}
 
-//   if (args[0] == NULL) {
-//     // An empty command was entered.
-//     return 1;
-//   }
-
-//   for (i = 0; i < lsh_num_builtins(); i++) {
-//     if (strcmp(args[0], builtin_str[i]) == 0) {
-//       return (*builtin_func[i])(args);
-//     }
-//   }
-
-//   return lsh_launch(args);
-// }
-
-// // void	evaluate_builtin()
-bool	exec_builtin(t_expression *expression, t_process *process, int cmd_idx, char *cmd)
+/*  return exit status  */
+int	exec_builtin(t_expression *expression, t_process *process, t_shell_var *shell_var)
 {
 	const char *builtin_str[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit"};
 	int (*builtin_func[]) (t_expression *, t_process *, int) = \
 			{&builtin_echo, &builtin_cd, &builtin_pwd, &builtin_export, \
 			&builtin_unset, &builtin_env, &builtin_exit};
-	int i;
+	char	*cmd;
+	int 	i;
 
+	cmd = process->command[0];
 	i = 0;
 	while (i < 7)
 	{
 		if (strcmp(cmd, builtin_str[i]) == 0)
-			exit((*builtin_func[i])(expression, process, cmd_idx));
+			return ((*builtin_func[i])(expression, process, shell_var));
 		i++;
 	}
-	return (false);
+	return (1);
 }
