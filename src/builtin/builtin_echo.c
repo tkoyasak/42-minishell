7f1d6917be31d6ea282b1153b3b64774732b43b9
Junya Kosaka <kosakajunya@gmail.com>
2022-03-14 01:33:32 +0900

    fix: builtin_echo -n

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -4,15 +4,25 @@ int	builtin_echo(t_expression *expression, t_process *process, t_shell_var *shel
 {
 	t_list			*itr;
 	char			*str;
+	bool			has_endl;
 
+	has_endl = true;
 	itr = process->token_list->next;
+	str = ((t_token *)(itr->content))->str;
+	if (ft_strcmp(str, "-n") == 0)
+	{
+		has_endl = false;
+		itr = itr->next;
+	}
 	while (itr)
 	{
 		str = ((t_token *)(itr->content))->str;
 		itr = itr->next;
+		printf("%s", str);
 		if (itr)
-			str = ft_strjoin(str, " ");
-		printf("%s\n", str);
+			printf(" ");
 	}
+	if (has_endl)
+		printf("\n");
 	return (0);
 }
