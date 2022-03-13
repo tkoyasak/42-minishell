2f05298f1663e965f3964a95228dd7257cf21039
Junya Kosaka <kosakajunya@gmail.com>
2022-03-13 10:48:52 +0900

    refactor: get_fullpath_cmdをbuiltinの判定後に変更

--- src/execution/exec_processes.c
+++ src/execution/exec_processes.c
@@ -160,11 +160,11 @@ void	exec_child(t_expression *expression, t_process *process, const int cmd_idx,
 
 	cmd = ((t_token *)(process->token_list->content))->str;
 	// builtinの判定
-	fullpath_cmd = get_fullpath_cmd(cmd);
 	dup2_func(expression, process, cmd_idx);
 	close_func(expression, process, cmd_idx);
 	if (is_builtin(cmd))
 		exit(exec_builtin(expression, process, shell_var));
+	fullpath_cmd = get_fullpath_cmd(cmd);
 	execve(fullpath_cmd, process->command, get_environ());
 	exit(NOCMD);
 }
