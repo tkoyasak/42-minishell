c5545eab365fe0c29d410cae7834a514f79cfd7f
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-15 18:42:12 +0900

    fix: $PWD,$OLDPWDの更新 - cd後にPWD,OLDPWDを更新する - "unset HOME"後に"cd"を実行したときのエラー処理を追加

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -1,15 +1,40 @@
 #include "minishell.h"
 
+int		builtin_cd_pwd_update(char *path_name, t_shell_var *shell_var)
+{
+	char *relative_path;
+
+	shell_var->oldpwd = shell_var->pwd;
+	if (getcwd(NULL, 0) == NULL)
+	{
+		ft_putendl_fd("cd: error retrieving current directory: \
+			getcwd: cannot access parent directories: \
+			No such file or directory", STDERR_FILENO);
+		relative_path = ft_strjoin("/", path_name);
+		shell_var->pwd = ft_strjoin(shell_var->pwd, relative_path);
+		free(relative_path);
+	}
+	else
+		shell_var->pwd = getcwd(NULL, 0);
+	free(path_name);
+	set_env_value("OLDPWD", shell_var->oldpwd, shell_var);
+	set_env_value("PWD", shell_var->pwd, shell_var);
+	return (0);
+}
+
 int		builtin_cd(t_expression *expression, t_process *process, t_shell_var *shell_var)
 {
 	char	*path_name;
-	char	*relative_path;
-	char	*tmp_path;
 
 	path_name = process->command[1];
 	if (path_name == NULL)
 	{
 		path_name = get_env_value("HOME", shell_var);
+		if (*path_name == '\0')
+		{
+			ft_putendl_fd("cd: HOME not set", STDERR_FILENO);
+			return (1);
+		}
 	}
 	if (chdir(path_name) == -1)
 	{
@@ -19,15 +44,5 @@ int		builtin_cd(t_expression *expression, t_process *process, t_shell_var *shell
 		free(path_name);
 		return (1);
 	}
-	// shell_var->pwd = getcwd(NULL, 0);
-	if (getcwd(NULL, 0) == NULL)
-	{
-		ft_putendl_fd("cd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory", STDERR_FILENO);
-		relative_path = ft_strjoin("/", path_name);
-		shell_var->pwd = ft_strjoin_free(shell_var->pwd, relative_path, true);
-	}
-	else
-		shell_var->pwd = getcwd(NULL, 0);
-	free(path_name);
-	return (0);
+	return (builtin_cd_pwd_update(path_name, shell_var));
 }
