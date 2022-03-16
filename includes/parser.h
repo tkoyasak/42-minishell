1123589085b3a89ed338632976467a847d8b9296
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-16 17:36:42 +0900

    feat: subshell_treeを作成

--- includes/parser.h
+++ includes/parser.h
@@ -3,14 +3,17 @@
 
 # include "libft.h"
 # include "lexer.h"
+// # include "execution.h"
 
 typedef enum e_node_kind
 {
+	ND_SUBSHELL,
 	ND_SEMICOLON, // ;
 	ND_DAND, // &&
 	ND_DPIPE, // ||
 	ND_PIPE, // |
-	ND_PROCESS // 文字列
+	ND_PROCESS, // 文字列
+	ND_EXPRESSION
 }	t_node_kind;
 
 // 抽象構文木のノードの型
@@ -18,6 +21,7 @@ typedef struct s_node
 {
 	t_node_kind		kind; // ノードの型
 	t_list			*token_list; // ls -> -al の 先頭(ls)
+	// t_expression	*expression; // ls -al | echo | 'hello'
 	struct s_node	*lhs; // 左辺
 	struct s_node	*rhs; // 右辺
 }	t_node;
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,5 +1,7 @@
 #include "minishell.h"
 
+static t_node	*create_astree(t_list **itr);
+
 static t_node	*node_new(t_node_kind kind, t_node *lhs, t_node *rhs)
 {
 	t_node	*node;
@@ -34,7 +36,9 @@ static t_node	*create_process_node(t_list **itr)
 	node->kind = ND_PROCESS;
 	node->token_list = *itr;
 	while ((*itr)->next != NULL && \
-		((t_token *)((*itr)->next->content))->kind != TK_PROCESS_DELIM)
+		((t_token *)((*itr)->next->content))->kind != TK_PROCESS_DELIM && \
+		((t_token *)((*itr)->next->content))->kind != TK_L_PARENTHESIS && \
+		((t_token *)((*itr)->next->content))->kind != TK_R_PARENTHESIS)
 	{
 		if (((t_token *)(*itr)->content)->kind == TK_REDIRECT && \
 				((t_token *)((*itr)->next->content))->kind == TK_REDIRECT)
@@ -61,6 +65,29 @@ static t_list	**expect_process(t_list **itr)
 	return (itr);
 }
 
+static t_node	*create_subshell_tree(t_list **itr)
+{
+	t_node	*node;
+
+	if (consume_node_kind(itr, "("))
+	{
+		node = ft_calloc(1, sizeof(t_node));
+		node->kind = ND_SUBSHELL;
+		// printf("%s\n", ((t_token *)((*itr)->content))->str);
+		node->lhs = create_astree(itr);
+		// printf("%s\n", ((t_token *)((*itr)->content))->str);
+		if (consume_node_kind(itr, ")") == false)
+		{
+			ft_putstr_fd("syntax error near unexpected token `", STDERR_FILENO);
+			ft_putstr_fd(((t_token *)((*itr)->content))->str, STDERR_FILENO);
+			ft_putstr_fd("'\n", STDERR_FILENO);
+			exit(1); // error_handler();
+		}
+		return (node);
+	}
+	return (create_process_node(expect_process(itr)));
+}
+
 // semicolon間の部分木
 static t_node	*create_sub_astree(t_list **itr)
 {
@@ -68,31 +95,30 @@ static t_node	*create_sub_astree(t_list **itr)
 
 	if (*itr == NULL)
 		return (NULL);
-	node = create_process_node(expect_process(itr));
+	node = create_subshell_tree(itr);
 	while (true)
 	{
 		if (consume_node_kind(itr, "|"))
-			node = node_new(ND_PIPE, node, \
-					create_process_node(expect_process(itr)));
+			node = node_new(ND_PIPE, node, create_subshell_tree(itr));
 		else
 			return (node);
 	}
 }
 
 // 全体のrootのnodeへのポインタを返す
-static t_node	*create_astree(t_list *itr)
+static t_node	*create_astree(t_list **itr)
 {
 	t_node		*node;
 
-	node = create_sub_astree(&itr);
+	node = create_sub_astree(itr);
 	while (true)
 	{
-		if (consume_node_kind(&itr, ";"))
-			node = node_new(ND_SEMICOLON, node, create_sub_astree(&itr));
-		else if (consume_node_kind(&itr, "&&"))
-			node = node_new(ND_DAND, node, create_sub_astree(&itr));
-		else if (consume_node_kind(&itr, "||"))
-			node = node_new(ND_DPIPE, node, create_sub_astree(&itr));
+		if (consume_node_kind(itr, ";"))
+			node = node_new(ND_SEMICOLON, node, create_sub_astree(itr));
+		else if (consume_node_kind(itr, "&&"))
+			node = node_new(ND_DAND, node, create_sub_astree(itr));
+		else if (consume_node_kind(itr, "||"))
+			node = node_new(ND_DPIPE, node, create_sub_astree(itr));
 		else
 			return (node);
 	}
@@ -120,7 +146,7 @@ t_node	*parser(char *argv)
 	t_node	*tree;
 
 	token_list = lexer(argv);
-	tree = create_astree(token_list);
+	tree = create_astree(&token_list);
 	// dfs(tree);
 	return (tree);
 }
