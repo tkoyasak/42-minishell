ee48f86a59ee4b63cf7c2af55d91b29789d0d820
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-21 18:26:52 +0900

    fix: パスがディレクトリのときのエラー

--- src/execution/exec_processes.c
+++ src/execution/exec_processes.c
@@ -186,6 +186,7 @@ void	exec_child(t_expression *expression, t_process *process, const int cmd_idx,
 {
 	char	*cmd;
 	char	*fullpath_cmd;
+	struct stat	buf;
 
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
@@ -195,6 +196,17 @@ void	exec_child(t_expression *expression, t_process *process, const int cmd_idx,
 	if (is_builtin(cmd))
 		exit(exec_builtin(process, shell_var));
 	fullpath_cmd = get_fullpath_cmd(cmd, shell_var);
+	if (fullpath_cmd)
+	{
+		stat(fullpath_cmd, &buf);
+		if ((buf.st_mode & S_IFMT) == S_IFDIR)
+		{
+			ft_putstr_fd("minishell: ", STDERR_FILENO);
+			ft_putstr_fd(fullpath_cmd, STDERR_FILENO);
+			ft_putendl_fd(": is a directory", STDERR_FILENO);
+			exit(126);
+		}
+	}
 	execve(fullpath_cmd, process->command, get_environ(shell_var));
 	exit(NOCMD);
 }
