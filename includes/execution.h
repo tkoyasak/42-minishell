98d99af984b6cbcdf5c76016218e484e3920cc51
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-22 15:02:15 +0900

    refactor: heredocのファイルを分割

--- includes/execution.h
+++ includes/execution.h
@@ -19,7 +19,7 @@ int			evaluate_expression(t_expression *expression, t_shell_var *shell_var);
 char		*get_fullpath_cmd(char *cmd, t_shell_var *shell_var);
 int			execution(t_node *tree, t_shell_var *shell_var);
 int			set_heredoc(t_node *tree, t_shell_var *shell_var);
-void		set_heredoc_sub(t_process *process, char *limiter, t_shell_var *shell_var);
+// void		set_heredoc_sub(t_process *process, char *limiter, t_shell_var *shell_var);
 int			exec_builtin(t_process *process, t_shell_var *shell_var);
 bool		is_builtin(char *cmd);
 int			exec_processes(t_expression *expression, t_shell_var *shell_var);
@@ -39,6 +39,14 @@ void	set_command(t_process *process);
 void	set_redirections_and_commands(t_expression *expression, t_shell_var *shell_var);
 void	exec_child(t_expression *expression, t_process *process, const int cmd_idx, t_shell_var *shell_var);
 
+
+size_t	get_word_len_heredoc(char *str, bool in_squote, bool in_dquote);
+t_list	*extract_word_heredoc(char **str, bool in_squote, bool in_dquote, t_expansion_kind kind);
+t_list	*split_str_heredoc(char *str, bool par_in_dquote);
+t_list	*get_expansion_list_heredoc(char *str, bool par_in_dquote, t_shell_var *shell_var);
+size_t	get_expanded_len_heredoc(t_list *exp_list);
+char	*expansion_heredoc(char *str, t_shell_var *shell_var);
+char	*remove_quote_heredoc(char *limiter, bool *in_quote);
 
 
 #endif
