1ff1f4be3cc759ad14680499c597caabbb48d9b8
Junya Kosaka <kosakajunya@gmail.com>
2022-03-23 01:16:03 +0900

    refactor: shell_varの引数を除去

--- includes/execution.h
+++ includes/execution.h
@@ -26,7 +26,6 @@ int			exec_builtin(t_process *process, t_shell_var *shell_var);
 bool		is_builtin(char *cmd);
 int			exec_processes(t_expression *expression, t_shell_var *shell_var);
 int			exec_single_process(t_expression *expression, t_shell_var *shell_var);
-void		set_redirections_and_commands(t_expression *expression, t_shell_var *shell_var);
 void		exec_child(t_expression *expression, t_process *process, const int cmd_idx, t_shell_var *shell_var);
 t_redirection_kind	get_redirection_kind(char *redirect_str);
 void		dup2_func(t_expression *expression, t_process *process, const int cmd_idx);
@@ -36,9 +35,9 @@ void		close_func(t_expression *expression, t_process *process, const int cmd_idx
 t_redirection_kind	get_redirection_kind(char *redirect_str);
 void		remove_redirection_token(t_process *process);
 void		open_error_handler(char *filename);
-void		set_redirection_params(t_process *process, t_shell_var *shell_var);
+void		set_redirection_params(t_process *process);
 void		set_command(t_process *process);
-void		set_redirections_and_commands(t_expression *expression, t_shell_var *shell_var);
+void		set_redirections_and_commands(t_expression *expression);
 
 /*  heredoc  */
 size_t		get_word_len_heredoc(char *str, bool in_squote, bool in_dquote);
