0d01383726295ec61dc735244acb5413f46122cb
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-03-28 11:37:41 +0900

    feat: add detect_leak

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: tkoyasak <tkoyasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 14:22:15 by jkosaka           #+#    #+#             */
-/*   Updated: 2022/03/25 22:55:42 by tkoyasak         ###   ########.fr       */
+/*   Updated: 2022/03/28 11:36:43 by tkoyasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,7 @@ char		*ft_xstrjoin_free(char *s1, char *s2, bool is_s2_free);
 char		**ft_xsplit(char *s, char c);
 char		*ft_xitoa(int n);
 t_list		*ft_xlstnew(void *content);
+
+void		detect_leak(int line, char *file); //削除
 
 #endif
--- src/utils/safe_func.c
+++ src/utils/safe_func.c
@@ -6,7 +6,7 @@
 /*   By: tkoyasak <tkoyasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 11:05:47 by tkoyasak          #+#    #+#             */
-/*   Updated: 2022/03/25 22:55:35 by tkoyasak         ###   ########.fr       */
+/*   Updated: 2022/03/28 11:36:09 by tkoyasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,26 @@ ssize_t	safe_func(ssize_t res)
 	if (res == -1)
 		exit(EXIT_FAILURE);
 	return (res);
+}
+
+void	detect_leak(int line, char *file) //削除
+{
+	char	buf[100];
+	int		status;
+
+	fprintf(stderr, "%s:%d\n", file, line);
+	(void)line;
+	(void)file;
+	snprintf(buf, 50, "leaks -q %d &> leaks.out", getpid());
+	// fprintf(stderr, "calling leaks command...\n");
+	status = system(buf);
+	if (!status)
+	{
+		fprintf(stderr, GREEN"OK! No leak detected.\n"RESET);
+	}
+	else
+	{
+		fprintf(stderr, RED"KO! LEAK DETECTED!\n"RESET);
+		system("cat leaks.out > /dev/stderr");
+	}
 }
